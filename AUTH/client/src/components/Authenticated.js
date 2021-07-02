06a4478f973310761bee278a7e68130a13e1b99d
Alifi Cleiton <alificleiton123@gmail.com>
2021-07-02 10:48:22 -0300

    Adicionando arquivos

---
+++ AUTH/client/src/components/Authenticated.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default (context ) => {
+
+  return (
+    <div className="bounds">
+      <div className="grid-100">
+        <h1> is authenticated!</h1>
+        <p>Your username is .</p>
+      </div>
+    </div>
+  );
+}
